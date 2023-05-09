61cb7b9b9ca7762424e7a20dd59696190eef211c
Lucie Kubatova <lucie.kubatova@hotmail.com>
2023-05-09 11:31:59 +0200

    app.post check

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,16 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    return res.send("Hello World");
+});
+app.post("/api/actions", (req, res) => {
+    const { action } = req.body;
+    if (action) {
+        return res.status(200).json({ action });
+    }
+    return res.status(400).send("Something went wrong");
 });
 const port = process.env.PORT;
 app.listen(port, () => {
--- index.ts
+++ index.ts
@@ -3,13 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app: Express = express();
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello World");
+app.get("/", (req: Request, res: Response): Response => {
+	return res.send("Hello World");
 });
 
-const port = process.env.PORT;
+app.post("/api/actions", (req: Request, res: Response) => {
+	const { action }: { action: string } = req.body;
+	if (action) {
+		return res.status(200).json({ action });
+	}
+	return res.status(400).send("Something went wrong");
+});
 
-app.listen(port, () => {
+const port: string | undefined = process.env.PORT;
+
+app.listen(port as string, (): void => {
 	console.log(`Listening on port ${port}`);
 });
